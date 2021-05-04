84c8b6d98f74f3f2ee1701cc73ab07bd49d6b915
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-04 17:04:38 +0530

    Header Component Styles

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,24 @@ import { View, StyleSheet, Text, ProgressViewIOSComponent } from "react-native";
 
 const Header = (props) => {
   return (
-    <View style={StyleSheet.headerContainer}>
-      <Text>{props.title}</Text>
+    <View style={styles.headerContainer}>
+      <Text style={styles.headerTitle}>{props.title}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    width: "100%",
+    height: 100,
+    backgroundColor: "#ffdde1",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerTitle: {
+    fontSize: 18,
+    paddingTop: 20,
+  },
+});
 
 export default Header;
