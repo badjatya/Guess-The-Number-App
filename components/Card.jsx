1dae6694598bd0d688c117cd9be19ce7ee796576
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-04 20:43:33 +0530

    Card component Styles

--- components/Card.jsx
+++ components/Card.jsx
@@ -7,6 +7,20 @@ const Card = props => {
     )
 }
 
-
+const styles = StyleSheet.create({
+    card:{
+        shadowColor: "black",
+        shadowOffset: {
+        width: 0,
+        height: 2,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: "white",
+        elevation: 5,
+        padding: 20,
+        borderRadius: 10,
+    }
+});
 
 export default Card;
