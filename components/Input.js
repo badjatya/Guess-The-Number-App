3a68768cff107ac7c788b28fe91865ba04305e83
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-05 22:37:33 +0530

    Input Container Styles

--- components/Input.js
+++ components/Input.js
@@ -2,9 +2,16 @@ import React from "react";
 import { StyleSheet, TextInput } from "react-native";
 
 const Input = (props) => {
-  return <TextInput {...props} />;
+  return <TextInput {...props} style={styles.input} />;
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    height: 30,
+    marginVertical: 10,
+    borderBottomColor: "grey",
+    borderBottomWidth: 2,
+  },
+});
 
 export default Input;
