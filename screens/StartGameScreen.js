ca5f48500e72be7f8890aa07fe44ea4743f87fa5
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-06 23:12:31 +0530

    Auto Capitalize

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,7 +11,7 @@ const StartGameScreen = (props) => {
       <Text style={styles.title}>Start Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Enter a Number</Text>
-        <Input style={styles.inputField} blurOnSubmit autoCapitalize={false} />
+        <Input style={styles.inputField} blurOnSubmit autoCapitalize="none" />
         <View style={styles.buttonContainer}>
           <View>
             <Button
