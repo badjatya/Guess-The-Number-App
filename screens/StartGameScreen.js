bb44bbe209a385a131769b93f17427576a577940
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-08 23:02:45 +0530

    Alert

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -31,6 +31,7 @@ const StartGameScreen = (props) => {
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
     if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid Number", "Number has to be between 1 and 99.");
       return;
     }
     setConfirmed(true);
