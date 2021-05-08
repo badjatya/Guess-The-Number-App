12b3d7c914a3505a40cf5e7d9e94c887ed0a5b5a
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-08 23:17:15 +0530

    NumberContainer

--- components/NumberContainer.jsx
+++ components/NumberContainer.jsx
@@ -1,3 +1,8 @@
 import React from "react";
-import {} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
 
+const NumberContainer = props => {
+    <View></View>
+}
+
+export default NumberContainer;
