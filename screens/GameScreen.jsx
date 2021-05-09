299e802874e6f77f50b4bbaf9798755ff0d57c40
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-09 20:29:36 +0530

    state added

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,27 +1,24 @@
-import React from "react";
-import {Text, View, StyleSheet} from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 const generateRandomBetween = (min, max, exclude) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
 
-    if(rndNum === exclude) {
-        return generateRandomBetween (min, max, exclude);
-    } 
-    else {
-        return rndNum;
-    }
-}
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
 
 const GameScreen = (props) => {
-    return (
-        <View style={styles.container}></View>
-    )
+  return <View style={styles.container}></View>;
 };
 
 const styles = StyleSheet.create({
-    container:{},
-})
+  container: {},
+});
 
 export default GameScreen;
