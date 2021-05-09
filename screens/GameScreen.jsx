be0bf139e9bec1d52db5b03bed3491dff4e77bdb
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-09 20:20:56 +0530

    generateRandomBetween

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import {Text, View, StyleSheet} from "react-native";
 
+const generateRandomBetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if(rndNum === exclude) {
+        return generateRandomBetween (min, max, exclude);
+    } 
+    else {
+        return rndNum;
+    }
+}
+
 const GameScreen = (props) => {
     return (
         <View style={styles.container}></View>
