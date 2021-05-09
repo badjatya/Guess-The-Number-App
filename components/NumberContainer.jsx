eefbba7232c1863dd39df121994eb9f47cd1fa4b
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-09 19:37:38 +0530

    NumberContainer

--- components/NumberContainer.jsx
+++ components/NumberContainer.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import {View, Text, StyleSheet} from "react-native";
 
 const NumberContainer = props => {
-    <View style={styles.container}></View>
+    <View style={styles.container}>
+        <Text>{props.children}</Text>
+    </View>
 }
 
 const styles = StyleSheet.create({
