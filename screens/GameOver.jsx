76895baef1ded51920eb3565399c71f4f92fa795
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-10 23:23:59 +0530

    GameOver

--- screens/GameOver.jsx
+++ screens/GameOver.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 const GameOver = (props) => {
-  return <View></View>;
+  return (
+    <View>
+      <Text>The Game is Over!</Text>
+    </View>
+  );
 };
 
 export default GameOver;
