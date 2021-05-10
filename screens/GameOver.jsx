b40a1ac6baea391fba7e9fd6a2a0e2cd67f98fd4
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-10 23:25:35 +0530

    GameOver component styles

--- screens/GameOver.jsx
+++ screens/GameOver.jsx
@@ -3,10 +3,18 @@ import { View, Text, StyleSheet } from "react-native";
 
 const GameOver = (props) => {
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>The Game is Over!</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default GameOver;
