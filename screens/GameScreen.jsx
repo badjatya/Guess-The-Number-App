99ab3ddab4fa4499a58db63f63d07f021d14eb80
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-10 22:16:53 +0530

    Imported Number Container

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 
+import NumberContainer from "../components/NumberContainer";
+
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -17,7 +19,11 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Text>Opponent's Guess</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
