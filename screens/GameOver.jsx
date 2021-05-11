00f7a8f5583e469736a326b10c497273f77b4f43
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-11 22:10:54 +0530

    Game over screen styling

--- screens/GameOver.jsx
+++ screens/GameOver.jsx
@@ -1,30 +1,40 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+
 import CustomButton from "../components/CustomButton";
 import Colors from "../constants/Colors";
+import Card from "../components/Card";
 
 const GameOver = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
-      <CustomButton style={styles.button} onPress={props.onRestart}>
-        NEW GAME
-      </CustomButton>
+      <Card style={styles.card}>
+        <Text>The Game is Over!</Text>
+        <Text>Number of rounds: {props.roundsNumber}</Text>
+        <Text>Number was: {props.userNumber}</Text>
+        <CustomButton style={styles.button} onPress={props.onRestart}>
+          NEW GAME
+        </CustomButton>
+      </Card>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
   button: {
     backgroundColor: Colors.primary,
     marginVertical: 10,
+  },
+  card: {
+    width: 300,
+    maxWidth: "80%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
   },
 });
 
