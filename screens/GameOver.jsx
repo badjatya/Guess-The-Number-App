25f438c45458fea3526e700221b682cb3d46f00c
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-11 21:15:32 +0530

    GameOver

--- screens/GameOver.jsx
+++ screens/GameOver.jsx
@@ -5,7 +5,7 @@ const GameOver = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The Game is Over!</Text>
-      <Text>Number of rounds: {props.roundNumber}</Text>
+      <Text>Number of rounds: {props.roundsNumber}</Text>
       <Text>Number was: {props.userNumber}</Text>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
