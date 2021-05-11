9daacbda58da1df77bb3206968ea482cb67ca3dd
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-11 22:14:26 +0530

    Game screen styling

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -90,6 +90,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: "center",
+    justifyContent: "center",
   },
   buttonContainer: {
     flexDirection: "column",
