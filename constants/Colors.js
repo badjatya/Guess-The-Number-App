289f90a49556fc1a0ddd93f07103b103328e5d2b
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-05-11 22:53:47 +0530

    Color update

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,10 +1,4 @@
 export default Colors = {
   primary: "#194350",
   secondary: "#28b5b5",
-  tertiary: "#100051",
-  four: "#8fd9a8",
-  five: "#ff8882",
-  six: "#ffc2b4",
-  seven: "#9dbeb9",
-  eight: "#bdc7c9",
 };
